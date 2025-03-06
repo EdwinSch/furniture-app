883c4daf3ae1cb93c71f6fe66f8fecd7458ed60c
Edwin <edwinschutjes@gmail.com>
2025-03-06 14:48:41 +0100

    implement sold feature

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
 
-const Card = ({ id, itemName, price, sold, photos, description }) => {
+const Card = ({ id, itemName, price, sold, photos }) => {
   return (
     <article className="card-wrapper">
+      {sold && (
+        <div className="card-disabled">
+          <p>Verkocht!</p>
+        </div>
+      )}
+
       <img src={photos[0].fields.file.url} className="img" alt={itemName} />
       <h3>{itemName}</h3>
       <p className="price">&euro; {price},-</p>
-      <Link to={`object/${id}`} className="link-btn">
+      <Link
+        to={`object/${id}`}
+        className={sold ? "link-btn disable-btn" : "link-btn"}
+      >
         Bekijk details
       </Link>
     </article>
