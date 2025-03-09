4c350c1b84d83887f035fcae02364bd0a2baa25e
Edwin <edwinschutjes@gmail.com>
2025-03-09 18:32:49 +0100

    some chagne

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
         Ik ga binnenkort verhuizen en daarom bied ik mijn meubels en inboedel
         tweedehands aan. Alles is van goede kwaliteit en in zeer goede staat.
         Heb je interesse? Stuur even een berichtje via deze site of klik door
-        naar mijn Martkplaats advertenties. Biedingen zjin mogelijk en voor
+        naar mijn Martkplaats advertenties. Biedingen zijn mogelijk en voor
         alles geldt: zelf ophalen in Breda.
       </p>
     </section>
